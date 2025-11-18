024c164cddef2f7c2f3a06ff537bf898d20ea0bb
John Brewer <jbrewer@jera.com>
2025-11-18 13:51:43 -0800

    Initial checkin

---
+++ AnchoringComponentTest/AnchoringComponentTestApp.swift
@@ -0,0 +1,34 @@
+//
+//  AnchoringComponentTestApp.swift
+//  AnchoringComponentTest
+//
+//  Created by John Brewer on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AnchoringComponentTestApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
