0452e2ef0a741a7a37f37efddf54db8f3d8a4554
John Brewer <jbrewer@jera.com>
2025-11-18 14:18:56 -0800

    Place shiny sphere on a table

--- AnchoringComponentTest/ImmersiveView.swift
+++ AnchoringComponentTest/ImmersiveView.swift
@@ -10,23 +10,23 @@ import RealityKit
 import GLTFKit2
 
 struct ImmersiveView: View {
+    @State var anchorEntity: AnchorEntity?
 
     var body: some View {
         RealityView { content in
             // Add the initial RealityKit content
             let urlResource = URLResource(name: "ShinySphere.glb")
-            if let url = URL(resource: urlResource),
-               let ball = try? await GLTFRealityKitLoader.load(from: url) {
-                let leftBall = ball
-                let rightBall = ball.clone(recursive: true)
-                leftBall.position = SIMD3<Float>(-0.5, 1.5, -1.5)
-                content.add(leftBall)
-                rightBall.position = SIMD3<Float>(0.5, 1.5, -1.5)
-                content.add(rightBall)
-
-                // Put skybox here.  See example in World project available at
-                // https://developer.apple.com/
+            guard let url = URL(resource: urlResource),
+                  let ball = try? await GLTFRealityKitLoader.load(from: url) else {
+                fatalError("Couldn't load ShinySphere.glb")
             }
+            ball.position = SIMD3<Float>(0, 0.1, 0)
+            anchorEntity = AnchorEntity(.plane(.horizontal, classification: .table, minimumBounds: [0.25, 0.25]))
+            anchorEntity?.addChild(ball)
+            content.add(anchorEntity!)
+
+            // Put skybox here.  See example in World project available at
+            // https://developer.apple.com/
         }
     }
 }
